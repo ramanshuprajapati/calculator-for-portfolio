f57bd5a30cdd06cc16cc4d373c23f96ca662aa37
ramanshuprajapati <ramanshugola@gmail.com>
2025-07-20 15:38:38 +0530

    i add local storage in it

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let getButtons = document.querySelectorAll("button");
 let getInput = document.querySelector("#display");
 
-
 let str = "";
+
+if(localStorage.getItem('data')){
+
+  str = localStorage.getItem('data');
+  getInput.value = str
+}
+
+
+
+
 getButtons.forEach(function (val) {
   val.addEventListener("click", function (dts) {
     if (dts.target.innerText == "=") {
       str = eval(str);
-   
+       localStorage.setItem('data',str);
       getInput.value = str;
+
     } else if (dts.target.innerText == "C") {
       str = "";
-
+      localStorage.setItem('data',str)
       getInput.value = str;
     } else if (dts.target.innerText == "⌫") {
       str = str.slice(0, str.length - 1);
